078e03dd761c454d85361d3053db1d2c14e03d92
Dev Vikram <harleysam12@gmail.com>
2024-12-19 15:55:35 +0530

    Update Uc3.jsx

--- src/components/Uc3.jsx
+++ src/components/Uc3.jsx
@@ -1,29 +1,32 @@
 import React, { useState } from 'react';
 // import { useState, useCallback, useMemo, memo, useEffect } from "react";
 
-function Ch3 ( { callbackFunction } ) {
+// Now Ch3 will not rerender if props is not changed
+const Ch3 = React.memo( ( { callbackFunction } ) => {
   const [ value, setValue ] = React.useState( 0 );
   console.log( "Child rerendered" );
 
   React.useEffect( () => {
     console.log( "callback inside child got changed" );
-    setValue( value + 1 );
+    // setValue( value + 1 );
   }, [ callbackFunction ] );
 
   return ( <p>Child: { value }</p> );
-}
+} );
 
 function Uc3 () {
   const [ count, setCount ] = React.useState( 0 );
   const [ another, setAnother ] = React.useState( 0 );
 
-  // const countCallback = React.useCallback( () => {
+  // const countCallback = () => {
   //   return count;
-  // }, [ count ] );
+  // };
 
-  const countCallback = () => {
+  // now the function will not rerender until dependancy is changed
+  const countCallback = React.useCallback( () => {
     return count;
-  };
+  }, [ count ] );
+
 
   return (
     <div>
